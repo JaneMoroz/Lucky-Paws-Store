e8afef3122145c3cb69e553e00c375b6bc418c58
Jane Moroz <Moroz.jane@yahoo.com>
2022-03-26 15:13:58 +0300

    Modified Navigation, Created Stories Section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let slideIndex = 1;
-let previousIndex;
+// let previousIndex;
+// let nextIndex;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -14,22 +15,25 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  previousIndex = n - 1;
+  // previousIndex = n - 1;
+  // nextIndex = n + 1;
   let slides = document.getElementsByClassName('slide');
   let dots = document.getElementsByClassName('dot');
   if (n > slides.length) {
     slideIndex = 1;
-    previousIndex = slides.length;
+    // previousIndex = slides.length;
+    // nextIndex = 2;
   }
   if (n < 1) {
     slideIndex = slides.length;
-    previousIndex = slides.length - 1;
+    // previousIndex = slides.length - 1;
+    // nextIndex = 1;
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.zIndex = '-1';
-    if (i !== previousIndex - 1) {
-      slides[i].style.display = 'none';
-    }
+    // if (i !== previousIndex - 1) {
+    //   slides[i].style.display = 'none';
+    // }
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(' active', '');
